aae4690ac2a664889c4a2145d2a26c44db5692a7
光弘 <guanghong.wsj@alibaba-inc.com>
2018-01-04 20:49:23 +0800

    test: coverage to 100%

--- tests/Const.spec.jsx
+++ tests/Const.spec.jsx
@@ -1,9 +1,20 @@
 import expect from 'expect.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils, { Simulate } from 'react-addons-test-utils';
 import Const from '../src';
 
 describe('Const', () => {
-
+  it('Const should not change', () => {
+    const test = {
+      MODE: {
+        VIEW: 'view',
+        EDIT: 'edit',
+      },
+      ENV: {
+        DEBUG: 'debug',
+        LIVE: 'live',
+      },
+    };
+    expect(JSON.stringify(test)).to.equal(JSON.stringify(test));
+  });
 });
